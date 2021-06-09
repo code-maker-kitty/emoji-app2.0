db7ea4c2dd2301029e6a61d355e43ff963534eec
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-06-09 12:26:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -26,3 +26,38 @@ function speak(){
     var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
 }
+function check(){
+    img=document.getElementsById('captured_image');
+    classifier.classify(img ,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementsById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementsById("result_emotion_name1").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label=="happy"){
+            document.getElementsById("update_emoji").innerHTML="&#128522;"
+        }
+        if(results[0].label=="sad"){
+            document.getElementsById("update_emoji").innerHTML="&#128546;"
+        }
+        if(results[0].label=="angry"){
+            document.getElementsById("update_emoji").innerHTML="&#128545;"
+        }
+        if(results[1].label=="happy"){
+            document.getElementsById("update_emoji2").innerHTML="&#128522;"
+        }
+        if(results[1].label=="sad"){
+            document.getElementsById("update_emoji2").innerHTML="&#128546;"
+        }
+        if(results[1].label=="angry"){
+            document.getElementsById("update_emoji2").innerHTML="&#128545;"
+        }
+    }
+}
